8f6b29b766db66c343c9e8118e0283dc83536863
dmunofer <diego.fmz@hotmail.com>
2022-03-21 13:31:46 +0100

    ej 1 modificado

--- HerenciaSimple.py
+++ HerenciaSimple.py
@@ -2,9 +2,10 @@ class Punto2D():
     def __init__(self,x,y):
         self.x=x
         self.y=y
-    def __str__(self):
-        str="A={}".format(self)
-        return str
+    def traslacion(self,p,q):
+        self.x=self.x+p
+        self.y=self.y+q
+        return self
 
 
 
