4ad91eb94fb07b62341fa57c8dcd0abb35721d9d
dmunofer <diego.fmz@hotmail.com>
2022-03-21 13:38:27 +0100

    ej1 con str

--- HerenciaSimple.py
+++ HerenciaSimple.py
@@ -2,11 +2,12 @@ class Punto2D():
     def __init__(self,x,y):
         self.x=x
         self.y=y
+    def __str__(self):
+        return "A= X : "(str(self.x)", Y: "str(self.y)
     def traslacion(self,p,q):
         self.x=self.x+p
         self.y=self.y+q
         return self
-
 
 
 
