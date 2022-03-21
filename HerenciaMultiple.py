eb504a3df298d4cd47820e328fdc4f1cef033c1f
dmunofer <diego.fmz@hotmail.com>
2022-03-21 14:49:42 +0100

    ej3 terminado bien

--- HerenciaMultiple.py
+++ HerenciaMultiple.py
@@ -1,14 +1,22 @@
 class A():
     def __init__(self,a):
         self.a =a 
+    def __str__(self):
+        return str(self.a)
 class B():
     def __init__(self,b):
         self.b=b 
+    def __str__(self):
+        return str(self.b)
 class C():
     def __init__(self,c):
         self.c =c 
+    def __str__(self):
+        return str(self.c)
 class D(A,B,C):
     def __init__(self,a,b,c):
-        self.a= A.__init__(self,a)
-        self.b=B.__init__(self,b)
-        self.c= C.__init__(self,c)
+        self.a= A(a)
+        self.b=B(b)
+        self.c= C(c)
+    def __str__(self):
+        return (str(self.a))+(str(self.b))+(str(self.c))
