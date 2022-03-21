7e13832bf35788c3dc8ec76d2f70a729b410c445
dmunofer <diego.fmz@hotmail.com>
2022-03-21 14:41:14 +0100

    fallo clase D

---
+++ HerenciaMultiple.py
@@ -0,0 +1,14 @@
+class A():
+    def __init__(self,a):
+        self.a =a 
+class B():
+    def __init__(self,b):
+        self.b=b 
+class C():
+    def __init__(self,c):
+        self.c =c 
+class D():
+    def __init__(self,a,b,c):
+        a= A(a)
+        b=B(b)
+        c= C(c)
