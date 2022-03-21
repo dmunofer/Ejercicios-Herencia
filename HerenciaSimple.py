8eba0d4ba3b05599e2af1247969911feafe1fc4a
dmunofer <diego.fmz@hotmail.com>
2022-03-21 13:22:53 +0100

    ej1

---
+++ HerenciaSimple.py
@@ -0,0 +1,19 @@
+class Punto2D():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def __str__(self):
+        str="A={}".format(self)
+        return str
+
+
+
+
+
+
+
+
+
+
+
+
